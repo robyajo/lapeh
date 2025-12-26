cb7d7e0121d92d575e43363175b7af9fcb29cc81
robyajo <robyfull.dev@gmail.com>
2025-12-26 10:26:50 +0700

    Fix Buug

--- src/middleware/error.ts
+++ src/middleware/error.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import { ZodError } from "zod";
-import { Prisma } from "../../generated/prisma/client";
+import { Prisma } from "@prisma/client";
 import { sendError } from "../utils/response";
 
 export function errorHandler(
